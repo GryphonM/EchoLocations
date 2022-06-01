6cd3bc525de280fdba4c10d02e0d30d0f6bc7339
tylerdeandigipen <71672056+tylerdeandigipen@users.noreply.github.com>
2022-05-31 21:38:34 -0700

    maze rot working
    marbleball scriopt needs to be integrated with the sound script thing

---
+++ EchoLocation/Assets/Scripts/MarbleScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarbleScript : MonoBehaviour
+{
+    public float speedToMakeNoise;
+    Rigidbody rb;
+    float timer = 0;
+    public float AntiNoiseSpamTimer;
+    bool delayOver = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        //ball wont move unless i do this nonsense \/
+        rb.isKinematic = true;
+        rb.isKinematic = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!delayOver)
+        {
+            timer += Time.deltaTime;
+        }
+        if (timer > AntiNoiseSpamTimer)
+        {
+            delayOver = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            if (rb.velocity.magnitude > speedToMakeNoise && delayOver)
+            {
+                Debug.Log("soundGo");
+                delayOver = false;
+            }
+        }
+    }
+}
